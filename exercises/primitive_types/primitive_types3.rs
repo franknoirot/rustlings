9f47ac2bccf50606458fb60f993afa5109702c99
Frank Johnson <frankjohnson1993@gmail.com>
2023-03-12 10:45:00 -0400

    Through lesson 39

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,9 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = "Something I think about often, is about how I don't think about stuff that often, you know? It's wild how much you can *not* think, you know?";
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
